55ac73ae2fc91336130c8b740a29815f07254daf
Marc Sibert <marc@sibert.fr>
2024-09-01 15:19:36 +0200

    AStyle & Licence

--- include/tokenizer.h
+++ include/tokenizer.h
@@ -1,77 +1,33 @@
+/**
+ * Copyright [2024] Marc SIBERT
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
 #pragma once
 
 #include <iostream>
 #include <string>
 #include <vector>
 #include <regex>
-#include "tokens.h"
+// #include "tokens.h"
+
+class Token;
 
 class Tokenizer
 {
     public:
-        std::vector<Token*> tokenize(const std::string& aLine) const {
-            static const std::regex space("^\\s.*");
-
-            std::vector<Token*> list;
-            auto pos = aLine.begin();
-            const auto end = aLine.end();
-
-            while (pos != end) {
-//                for (auto it=pos; it!=end; ++it) std::cerr << *it;
-//                std::cerr << std::endl;
-
-                if (std::regex_match(pos, end, space)) {
-                    pos++;
-                    continue;
-                }
-
-                auto pTCom = TokenComment::create(pos, end);
-                if (pTCom) {
-                    list.push_back(pTCom);
-                    continue;
-                }
-
-                auto pTIn = TokenInstruction::create(pos, end);
-                if (pTIn) {
-                    list.push_back(pTIn);
-                    continue;
-                }
-
-                auto pTId = TokenIdentifier::create(pos, end);
-                if (pTId) {
-                    list.push_back(pTId);
-                    continue;
-                }
-
-                auto pTOp = TokenOperator::create(pos, end);
-                if (pTOp) {
-                    list.push_back(pTOp);
-                    continue;
-                }
-
-                auto pTCo = TokenConstant::create(pos, end);
-                if (pTCo) {
-                    list.push_back(pTCo);
-                    continue;
-                }
-
-                auto pTSe = TokenSeparator::create(pos, end);
-                if (pTSe) {
-                    list.push_back(pTSe);
-                    continue;
-                }
-
-                std::cerr << "Syntax Error in line" << std::endl;
-                std::cerr << aLine << std::endl;
-                for (auto i = aLine.begin(); i != pos; ++i)
-                    std::cerr << " ";
-                std::cerr << "^" << std::endl;
-                break;
-
-            }
-
-            return list;
-        }
+        std::vector<Token*> tokenize(const std::string& aLine, bool& err, int& pos) const;
 
     protected:
 
