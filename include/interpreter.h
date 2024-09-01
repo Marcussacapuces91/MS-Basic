295dd6ffe301d28d3b2bc609258cbe2e9ebb8863
Marc Sibert <marc@sibert.fr>
2024-09-01 22:56:08 +0200

    Update interpreter.h

--- include/interpreter.h
+++ include/interpreter.h
@@ -1,42 +1,56 @@
 /**
  * Copyright [2024] Marc SIBERT
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  **/
- 
+
 #pragma once
 
+#include <cassert>
 #include <vector>
 #include <set>
+#include <iomanip>
 #include <iostream>
 #include <sstream>
 
 #include "tokenizer.h"
 #include "tokens.h"
 
-/*
-class Context {
-};
-*/
-
-class Command {
+/**
+ * A command is only on command, without ':' separator. It's possible to have many command in a line.
+ **/
+class Command : private std::vector<Token*> {
 	public:
-		Command(const std::vector<Token*>& aTokens) {}
+		Command(const std::vector<Token*>& aTokens) : std::vector<Token*>(aTokens) {}
 
-	protected:
-
-	private:
+	friend std::ostream& operator<<(std::ostream&, const Command&);
 };
+
+std::ostream& operator<<(std::ostream& out, const Command& aCommand) {
+	for (auto&& token : aCommand) {
+		out << (token == *aCommand.begin() ? "" : " ") << *token;
+	}
+	return out;
+}
+
+
+std::ostream& operator<<(std::ostream& out, const std::vector<Command>& aCommands) {
+	for (auto&& command : aCommands)	{
+		out << (&command == &(*aCommands.begin()) ? "" : " : ") << command;
+	}
+	return out;
+}
+
 
 class Interpreter {
 	public:
@@ -44,17 +58,17 @@ class Interpreter {
 			OK,
 			SYNTAX_ERROR
 		};
-		
+
 		Interpreter(std::istream& aIn = std::cin, std::ostream& aOut = std::cout, std::ostream& aErr = std::cerr) :
 			in(aIn),
 			out(aOut),
 			err(aErr) {
 		}
-		
+
 		error_t load(std::ifstream& aFile) {
-		    std::string line;
-		    while (std::getline(aFile, line)) {
-				// Empty line
+			std::string line;
+			while (std::getline(aFile, line)) {
+				// Empty line?
 				if (!line.length()) continue;
 
 				Tokenizer tokenizer;
@@ -62,6 +76,7 @@ class Interpreter {
 				int pos;
 				const auto tokens = tokenizer.tokenize(line, error, pos);		// need to free later all takens!!
 				if (error) {
+					assert(tokens.size() == 0);
 					err << "Syntax Error in:" << std::endl;
 					err << line << std::endl;
 					err << std::string(pos, ' ') << '^' << std::endl;
@@ -78,24 +93,34 @@ class Interpreter {
 						err << "Syntax Error: A line number must be an INTEGER!" << std::endl;
 						err << line << std::endl;
 						return SYNTAX_ERROR;
-					} 
+					}
 				} else {
 					err << "Syntax Error: A line number must be an INTEGER!" << std::endl;
 					err << line << std::endl;
 					return SYNTAX_ERROR;
 				}
+				assert(lineNumber);	// not 0
 
-				std::vector< std::vector<Token*> > commands;
+				std::vector<Command> commands;
 				while (token != tokens.end()) {
-					const std::vector<Token*> command = commandSlicer(token, tokens.end());
-					out << command << ':';
+					auto command = commandSlicer(token, tokens.end());
 					commands.push_back(command);
 				}
-				out << std::endl;
-		    }
+				out << std::setw(5) << lineNumber << " " << commands << std::endl;
+
+				
+	
+			}
+			return OK;
 		}
-		
-		std::vector<Token*> commandSlicer(std::vector<Token*>::const_iterator& start, const std::vector<Token*>::const_iterator& stop) {
+
+		/**
+		 * Slice each tokens' list in separate commands, using ':' separator.
+		 * @param start Iterator on first token.
+		 * @param stop Iterator after the last token.
+		 * @return A vector of tokens.
+		 **/
+		Command commandSlicer(std::vector<Token*>::const_iterator& start, const std::vector<Token*>::const_iterator& stop) {
 			std::vector<Token*> res;
 			for (auto token = start; token != stop; ++token) {
 				if (const auto pTSep = dynamic_cast<TokenSeparator*>(*token)) {
@@ -107,7 +132,7 @@ class Interpreter {
 				res.push_back(*token);
 			}
 			start = stop;
-			return res;
+			return Command(res);
 		}
 
 
@@ -126,8 +151,8 @@ class Interpreter {
 		std::istream& in;
 		std::ostream& out;
 		std::ostream& err;
-//    Context context;
-//		std::set<CommandLine> program;
+
+		std::map<unsigned, std::vector<Command> > program;
 };
 
 std::ostream& operator<<(std::ostream& out, const Interpreter& aInterpreter) {
