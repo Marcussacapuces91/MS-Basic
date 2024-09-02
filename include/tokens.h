7134bcfcc7d84144b25dd999b62cce2a12ea7905
Marc Sibert <marc@sibert.fr>
2024-09-02 19:50:00 +0200

    Update tokens.h

--- include/tokens.h
+++ include/tokens.h
@@ -1,19 +1,19 @@
 /**
  * Copyright [2024] Marc SIBERT
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  **/
- 
+
 #pragma once
 
 #include <ostream>
@@ -22,13 +22,14 @@
 
 class Token {
 	public:
-		friend std::ostream& operator<<(std::ostream&, const Token&);
 
 		///< To distinguish between String or Number identifier (with $ terminator).
 		enum type_t { STRING, INTEGER, SINGLE, DOUBLE, HEXADECIMAL, OCTAL, CHANEL };
 
 	protected:
 		virtual std::string toString() const = 0;
+
+		friend std::ostream& operator<<(std::ostream&, const Token&);
 };
 
 /**
@@ -177,7 +178,7 @@ class TokenSeparator : public Token {
 		 * Factory.
 		 */
 		static TokenSeparator* create(std::string::const_iterator& aStart, const std::string::const_iterator& aStop);
-		
+
 		const std::string& getId() const;
 
 	protected:
